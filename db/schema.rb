94895440d1f24894c6704d0a8c044eab0038f470
Kirill Zonov <graffzon@gmail.com>
2012-10-18 17:43:04 +0400

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018105906) do
+ActiveRecord::Schema.define(:version => 20121018133407) do
+
+  create_table "tasks", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "project"
+    t.string   "name"
+    t.float    "real_time"
+    t.float    "time_points"
+    t.string   "status"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "tasks", ["user_id"], :name => "index_tasks_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
