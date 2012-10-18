fb779546adf211e32227d15cfe656a872db858b2
Kirill Zonov <graffzon@gmail.com>
2012-10-18 11:51:57 +0400

    genered rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTrack::Application.config.secret_token = '797a83809215905d9a29affaef47afc61f1e495e15edcd8100d69fa344062649773f1154132d3228ed107597d78c9297389d12842ff110d1e6bdacc21fbc54c7'
