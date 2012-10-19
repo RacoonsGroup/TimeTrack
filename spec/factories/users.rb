02028a235fef1e35f8f3bc30db6034646744ae75
Kirill Zonov <graffzon@gmail.com>
2012-10-19 18:19:59 +0400

    added factory user

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    email "user@example.com"
+    role "user"
+  end
+
+  factory :admin do
+    email "admin@example.com"
+    role "admin"
+  end
+end
