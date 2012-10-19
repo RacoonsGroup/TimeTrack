42fb4c7c0b6dd28cd60cc86f210cabf37474a9d1
Andrey Nenashev <proudhamyak@gmail.com>
2012-10-19 18:04:32 +0400

    Create migration AddRoleToUser

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019133538) do
+ActiveRecord::Schema.define(:version => 20121019140240) do
 
   create_table "time_entries", :force => true do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20121019133538) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
