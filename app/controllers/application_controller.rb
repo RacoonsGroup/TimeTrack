f61d4ad2af29d012a89ade2935b6c879bed78e7e
Andrey Nenashev <proudhamyak@gmail.com>
2012-10-23 19:27:47 +0400

    Fixed filter authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate_user!
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
