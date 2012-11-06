3d5caaae7beadae09469441e31bd953c69d044c8
Kirill Zonov <graffzon@gmail.com>
2012-11-06 12:44:17 +0400

    added smtp to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ TimeTrack::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'task-track.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
 end
