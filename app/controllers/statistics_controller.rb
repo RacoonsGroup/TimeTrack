68fc8f57640910d57cfbc57812edea81ff958142
Kirill Zonov <graffzon@gmail.com>
2012-12-08 19:17:27 +0400

    fixed statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,17 +1,14 @@
 class StatisticsController < ApplicationController
 
   def index
-    @user_id = current_user.admin? ? ( params[:user_id] or current_user.id ) : current_user.id
-    @from = params[:from_date]
-    @to = params[:to_date]
+    user_id = current_user.admin? ? ( params[:user_id] || current_user.id ) : current_user.id
 
-    @page = params[:page] or 1
-    @time_entries = TimeEntry.in_date_range(@from, @to, @user_id).page(@page)
+    @time_entries = TimeEntry.in_date_range(params[:from_date], params[:to_date], user_id).page(params[:page])
     if current_user.admin?
       @users = User.all
     end
-    #TODO: SQL count?
-    @real_time = TimeEntry.all.collect(&:real_time).sum
-    @delivered_time = TimeEntry.all.collect(&:time_points).sum
+
+    @real_time = @time_entries.collect(&:real_time).compact.sum
+    @delivered_time = @time_entries.collect(&:time_points).compact.sum
   end
 end
