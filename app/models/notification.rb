436645206fcf245c06c9dc77914bb1a3b5f45827
Kirill Zonov <graffzon@gmail.com>
2013-04-09 16:27:09 +0400

    added mail-sending for notifies

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,10 @@ class Notification < ActiveRecord::Base
   attr_accessible :description, :end_date, :start_date, :title
 
   scope :current, -> { where("start_date <= '#{Time.zone.now}' AND end_date >= '#{Time.zone.now}'") }
+
+  after_create :notify_users
+
+  def notify_users
+    NotifyMailer.new_notify(self)
+  end
 end
