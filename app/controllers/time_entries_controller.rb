f0c100e9f0fb474851a431a89f3083ca50681f8b
Marat <marat@marat-HP-Pavilion-g6-Notebook-PC.(none)>
2013-06-05 12:25:50 +0400

    Add index, show, update, destroy for Admin role

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,6 +1,22 @@
 class TimeEntriesController < InheritedResources::Base
   include TimeEntriesHelper
   actions :all
+  
+  def index
+    if can?(:manage,:time_entries)
+      @time_entries= Kaminari.paginate_array(TimeEntry.all).page(params[:page])
+    else
+      index!{ collection_url }
+    end
+  end
+
+  def show
+    if can?(:manage,:time_entries)
+      @time_entry = TimeEntry.find(params[:id])
+    else
+      show!{ collection_url }
+    end
+  end
 
   def create
     params[:time_entry][:real_time] = pretty_time(params[:time_entry][:real_time])
@@ -12,13 +28,37 @@ class TimeEntriesController < InheritedResources::Base
   end
 
   def edit
-    edit!{ collection_url }
+    if can?(:manage,:time_entries)
+      @time_entry = TimeEntry.find(params[:id])
+    else
+      edit!{ collection_url }
+    end
   end
 
   def update
-    update!{ collection_url }
+    params[:time_entry][:real_time] = pretty_time(params[:time_entry][:real_time])
+    if can?(:manage,:time_entries)
+      @time_entry=TimeEntry.find(params[:id])
+      if @time_entry.update_attributes(params[:time_entry])
+        redirect_to @time_entry
+      else
+        #render action: "edit"
+      end
+    else
+      update!{ collection_url }
+    end
   end
-
+  
+  def destroy
+    if can?(:manage,:time_entries)
+      @time_entry=TimeEntry.find(params[:id])
+      @time_entry.destroy
+      redirect_to time_entries_url
+    else
+      destroy!{collection_url}
+    end
+  end
+  
   protected
     def begin_of_association_chain
       current_user
