c992e5803d485735bb8942a89f01889f07c1396e
Marat <marat@marat-HP-Pavilion-g6-Notebook-PC.(none)>
2013-06-10 13:59:11 +0400

    remove path

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.load_paths += %W(
-  {Rails.root}/app/presenters
-)
 # Initialize the rails application
 TimeTrack::Application.initialize!
