033b7b90d65f511887a4a51b04463714f78868c5
Marat <marat@marat-HP-Pavilion-g6-Notebook-PC.(none)>
2013-06-10 12:21:26 +0400

    presenters path add

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.load_paths += %W(
+  {Rails.root}/app/presenters
+)
 # Initialize the rails application
 TimeTrack::Application.initialize!
