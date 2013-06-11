49065718958d0667a7e14f92939f78bd8e4fb869
Marat <marat@marat-HP-Pavilion-g6-Notebook-PC.(none)>
2013-06-11 13:03:51 +0400

    Project facrory add

---
+++ spec/factories/projects.rb
@@ -0,0 +1,14 @@
+
+FactoryGirl.define do
+  factory :project do
+    project_name "project"
+    project_description "project description"
+    disable false
+  end
+
+  factory :disable_project do
+    project_name "project"
+    project_description "project description"
+    disable true
+  end
+end
