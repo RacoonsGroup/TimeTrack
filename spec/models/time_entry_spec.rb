e151fcae8839c4d194c39e6d163e315388db3718
Marat <marat@marat-HP-Pavilion-g6-Notebook-PC.(none)>
2013-06-11 13:14:03 +0400

    in_range move to filter, project validate add

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -19,18 +19,20 @@ describe TimeEntry do
   it { should ensure_length_of(:name).is_at_most(255) }
   #Now project is separate model
   #it { should ensure_length_of(:project).is_at_most(100) }
+  it { should belong_to(:project) }
   it { should validate_numericality_of(:real_time) }
   it { should ensure_inclusion_of(:status).in_array(TimeEntry::STATUSES) }
 
-  it 'should respond to ::in_range' do
-    TimeEntry.should respond_to(:in_range)
-    random_date = "09.09.2009".to_date
-    params={from_date: random_date, to_date: random_date, project_id: 1}
-    TimeEntry.in_range(user, params).should be_true
-    TimeEntry.in_range(user, params.merge(from_date: "")).should be_true
-    TimeEntry.in_range(user, params.merge(to_date: "")).should be_true
-    TimeEntry.in_range(user, params.merge(project_id: "")).should be_true
-    TimeEntry.in_range(admin, params.merge(user_id: user.id)).should be_true
-  end
+  # Now in filter
+  #it 'should respond to ::in_range' do
+  #  TimeEntry.should respond_to(:in_range)
+  #  random_date = "09.09.2009".to_date
+  #  params={from_date: random_date, to_date: random_date, project_id: 1}
+  #  TimeEntry.in_range(user, params).should be_true
+  #  TimeEntry.in_range(user, params.merge(from_date: "")).should be_true
+  #  TimeEntry.in_range(user, params.merge(to_date: "")).should be_true
+  #  TimeEntry.in_range(user, params.merge(project_id: "")).should be_true
+  #  TimeEntry.in_range(admin, params.merge(user_id: user.id)).should be_true
+  #end
 
 end
