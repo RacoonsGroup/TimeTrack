aed1abc0221f866e8d66d617bc66e8662dde94de
Mahys116 <mahys116@gmail.com>
2013-06-18 13:14:38 +0400

    Add type ti project. Can be only point level internal

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
-  attr_accessible :project_name, :project_description, :disable
+  attr_accessible :project_name, :project_description, :type, :disable
   has_many :time_entries
+
+  TYPES = %w(point level internal)
+
+  validates :type, inclusion: { in: TYPES }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130617091134) do
+ActiveRecord::Schema.define(:version => 20130618090905) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20130617091134) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.boolean  "disable"
+    t.string   "type"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
