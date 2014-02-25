c677ab15ee0360a41cbcc70ff6df712ea97532bc
Kseniya Sukhova <your.email@example.com>
2014-02-25 21:01:38 +0400

    Add multiform for User in filter, fixed bug registry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update).insert(-1, :name)
   end
 end
