db7a9f203f37a2e90e2da74894efb32c63d8939d
Kirill Zonov <graffzon@gmail.com>
2014-05-07 14:13:24 +0400

    disabled registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TimeTrack::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   resources :projects
